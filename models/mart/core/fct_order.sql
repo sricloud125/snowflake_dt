46955cf88476e34c9b0f737b55704f7f477f5bae
sricloud125@gmail.com <sricloud125@gmail.com>
2023-03-09 14:03:34 +0000

    .

--- models/mart/core/fct_order.sql
+++ models/mart/core/fct_order.sql
@@ -1,9 +1,9 @@
 with orders as (
-    select * from {{ ref('stg_order')}}
+    select * from {{ ref('stg_orders')}}
 ),
 
 payments as (
-    select * from {{ ref('stg_payment')}}
+    select * from {{ ref('stg_payments')}}
 ),
 
 fct_orders as (
